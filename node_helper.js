00c8e8c4b592cf54d12ab9964117de1e90e94103
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-05-08 05:29:01 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -30,7 +30,7 @@ module.exports = NodeHelper.create({
 	 */
 	getData: function() {
 		var self = this;
-
+		Log.info('Getting Data: ' + this.name);
 		var urlApi = this.config.urlApi; //"https://jsonplaceholder.typicode.com/posts/1";
 		var retry = true;
 		request({
@@ -40,6 +40,7 @@ module.exports = NodeHelper.create({
 			
 				if (!error && response.statusCode == 200) {
 					self.sendSocketNotification("DATA", body);
+					Log.info(body + ' : ' + this.name);
 				} else {
 					console.log("Error loading");
 				}
@@ -51,20 +52,5 @@ module.exports = NodeHelper.create({
 	// Example function send notification test
 	sendNotificationTest: function(payload) {
 		this.sendSocketNotification("MMM-RequestHTML-NOTIFICATION_TEST", payload);
-	},
-
-	// this you can create extra routes for your module
-	extraRoutes: function() {
-		var self = this;
-		this.expressApp.get("/MMM-RequestHTML/extra_route", function(req, res) {
-			// call another function
-			values = self.anotherFunction();
-			res.send(values);
-		});
-	},
-
-	// Test another function
-	anotherFunction: function() {
-		return {date: new Date()};
 	}
 });
